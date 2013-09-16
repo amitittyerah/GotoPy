e8b3de03a2b3936d9d445a04b1a0524e166c79d6
Amit <admin@devscoop.com>
2013-09-16 14:00:23 +1000

    list,flush

--- src/goto.py
+++ src/goto.py
@@ -34,19 +34,40 @@ def _uninstall():
     os.system("rm %s" % bash_py_src)
     os.system("rm %s" % py_src)
 
+def _read_cache():
+    content = False
+    if os.path.exists(cache_file):
+        with open(cache_file) as f:
+                content = f.readlines()
 
+    print "Cache is empty" if not content else "..cache read"
+
+    return content
 
 def _key_exists(new_line):
-    with open(cache_file) as f:
-        content = f.readlines()
+    content = _read_cache()
     for line in content:
         line_comp = line.split("::")
-        new_line_comp = new_line.split("::")
-        if len(new_line_comp) != 3:
-            return "Format needed - path::cmd::key"
-        if new_line_comp[0] in line_comp[0]:
-            return "Already exists as %s::%s::%s" % (line_comp[0],line_comp[1],line_comp[2])
+        if len(line_comp) == 3:
+            new_line_comp = new_line.split("::")
+            if len(new_line_comp) != 3:
+                return "Format needed - path::cmd::key"
+            if str(new_line_comp[0]).strip() == str(line_comp[0]).strip():
+                return "Already exists as %s::%s::%s" % (line_comp[0],line_comp[1],line_comp[2])
+            elif str(new_line_comp[2]).strip() == str(line_comp[2]).strip():
+                return "Conflicts with  %s::%s::%s" % (line_comp[0],line_comp[1],line_comp[2])
     return False
+
+def _flush():
+    if os.path.exists(cache_file):
+        os.system("rm %s" % cache_file)
+
+def _list():
+    content = _read_cache()
+    if content:
+        for line in content:
+            print line
+
 
 if len(sys.argv) == 2:
 
@@ -55,23 +76,30 @@ if len(sys.argv) == 2:
         print "Installed. Please restart your terminal"
         os.system("exit")
         sys.exit(1)
-    if sys.argv[1] == "uninstall":
+    elif sys.argv[1] == "uninstall":
         _uninstall()
         print "Uninstalled. Please remove the line from your .bash_profile"
         sys.exit(1)
 
-    _setup()
+    elif sys.argv[1] == "flush":
+        _flush()
+        print "Cache cleared"
 
-    with open(cache_file) as f:
-        content = f.readlines()
+    elif sys.argv[1] == "list":
+        _list()
 
-    for line in content:
-        line_comp = line.split("::")
-        if(len(line_comp) == 3):
-            if sys.argv[1] in line_comp[2]:
-                cmd = "%s %s" % (line_comp[1],line_comp[0])
-                os.system("echo %s | pbcopy" % cmd)
-                print "Paste it now : %s" % cmd
+    else:
+        _setup()
+        content = _read_cache()
+        if content:
+            for line in content:
+                line_comp = line.split("::")
+                if(len(line_comp) == 3):
+                    if str(sys.argv[1]).strip() == str(line_comp[2]).strip():
+                        cmd = "%s %s" % (line_comp[1],line_comp[0])
+                        os.system("echo %s | pbcopy" % cmd)
+                        print "Paste it now : %s" % cmd
+
 
 elif len(sys.argv) == 3:
     _setup()
@@ -80,4 +108,4 @@ elif len(sys.argv) == 3:
         _add_to_file(cache_file,sys.argv[2])
         print "Added"
     else:
-        print "Error as %s" % out
+        print "Error : %s" % out
